f8766cb1c3542f39e27dfcf3f98f468aa0f5dd87
ninolin <a7872278722@gmail.com>
2018-09-05 00:09:52 +0800

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AppNavbarComponent } from './app-navbar/app-navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AppNavbarComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
